af3fe7ea1d0a5ea21f48b54295e58b6bfa4d3a04
ShanKwanCho <scho4@mail.sfsu.edu>
2020-06-11 22:11:48 -0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC5K7GU7U7VepoXYmuDK4nVHXN31apWJ_I",
+    authDomain: "todolist-ssdk22.firebaseapp.com",
+    databaseURL: "https://todolist-ssdk22.firebaseio.com",
+    projectId: "todolist-ssdk22",
+    storageBucket: "todolist-ssdk22.appspot.com",
+    messagingSenderId: "28726958269",
+    appId: "1:28726958269:web:5d5b5285bdbb869438bfd4",
+    measurementId: "G-S02K26B0N7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC5K7GU7U7VepoXYmuDK4nVHXN31apWJ_I",
+    authDomain: "todolist-ssdk22.firebaseapp.com",
+    databaseURL: "https://todolist-ssdk22.firebaseio.com",
+    projectId: "todolist-ssdk22",
+    storageBucket: "todolist-ssdk22.appspot.com",
+    messagingSenderId: "28726958269",
+    appId: "1:28726958269:web:5d5b5285bdbb869438bfd4",
+    measurementId: "G-S02K26B0N7"
+  }
 };
 
 /*
